922e028279638bbb1b51074c1832ded3ceaa4f4d
Adam Breaux <breauxaj@gmail.com>
2016-05-30 15:43:25 -0400

    another common piece

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,12 @@ class template (
   $ensure = 'latest'
 ) inherits ::template::params {
   package { $::template::params::template_package:
-    ensure  => $ensure,
+    ensure => $ensure,
+  }
+
+  service { $::template::params::template_service:
+    ensure => 'running',
+    enable => true,
   }
 
 }
